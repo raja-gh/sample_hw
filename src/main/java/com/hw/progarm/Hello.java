b9b539d0076b0cb5604b5272c6c65dcf31e386e7
raja-gh <157103669+raja-gh@users.noreply.github.com>
2024-04-17 11:09:30 +0530

    Update Hello.java

--- src/main/java/com/hw/progarm/Hello.java
+++ src/main/java/com/hw/progarm/Hello.java
@@ -1,9 +1,9 @@
-package com.hw.progarm;
+// package com.hw.progarm;
 
-public class Hello {
+// public class Hello {
 
 	
-	public static void main(String args[]) {
-		System.out.println("Hello World!!!!");
-	}
-}
+// 	public static void main(String args[]) {
+// 		System.out.println("Hello World!!!!");
+// 	}
+// }
